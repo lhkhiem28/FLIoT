28fff0a25fbcab6342bfeaac767143a921dc76f2
lhkhiem28 <lhkhiem28@gmail.com>
2023-01-12 09:04:11 +0000

    baseline

--- source/partition.py
+++ source/partition.py
@@ -5,7 +5,7 @@ from libs import *
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_dir", type = str), parser.add_argument("--num_classes", type = int)
 parser.add_argument("--num_clients", type = int, default = 64)
-parser.add_argument("--non_iid", action = "store_true")
+parser.add_argument("--non_iid", action = "store_true"), parser.add_argument("--num_chunks", type = int, default = 4)
 args = parser.parse_args()
 
 if not os.path.exists(args.data_dir + "clients/"):
@@ -17,19 +17,33 @@ if not os.path.exists(args.data_dir + "clients/"):
 image_files = glob.glob(args.data_dir + "train/*/*")
 if not args.non_iid:
     np.random.shuffle(image_files)
-    num_examples_per_client = len(image_files) // args.num_clients
+
+    num_examples_per_chunk = len(image_files) // args.num_clients
+    chunks = list(range(args.num_clients))
+    for c, chunk in enumerate(chunks):
+        client_image_files = image_files[num_examples_per_chunk*(chunk):num_examples_per_chunk*(chunk + 1)]
+        np.random.shuffle(client_image_files)
+        for image_file in client_image_files[:int(num_examples_per_chunk*0.9)]:
+            label = int(image_file.split("/")[-2])
+            shutil.copy(image_file, args.data_dir + "clients/c{}/fit/{}/".format(str(c), str(label)))
+        for image_file in client_image_files[int(num_examples_per_chunk*0.9):]:
+            label = int(image_file.split("/")[-2])
+            shutil.copy(image_file, args.data_dir + "clients/c{}/evaluate/{}/".format(str(c), str(label)))
 else:
     image_files = [image_file for _, image_file in sorted(zip([int(image_file.split("/")[-2]) for image_file in image_files], image_files))]
-    num_examples_per_client = len(image_files) // args.num_clients
 
-chunks = list(range(args.num_clients))
-np.random.shuffle(chunks)
-for c, chunk in enumerate(chunks):
-    client_image_files = image_files[num_examples_per_client*(chunk):num_examples_per_client*(chunk + 1)]
-    np.random.shuffle(client_image_files)
-    for image_file in client_image_files[:int(num_examples_per_client*0.9)]:
-        label = int(image_file.split("/")[-2])
-        shutil.copy(image_file, args.data_dir + "clients/c{}/fit/{}/".format(str(c), str(label)))
-    for image_file in client_image_files[int(num_examples_per_client*0.9):]:
-        label = int(image_file.split("/")[-2])
-        shutil.copy(image_file, args.data_dir + "clients/c{}/evaluate/{}/".format(str(c), str(label)))
+    num_examples_per_chunk = len(image_files) // (args.num_clients*args.num_chunks)
+    chunks = list(range(args.num_clients*args.num_chunks))
+    np.random.shuffle(chunks)
+    for c in range(args.num_clients):
+        client_chunks = chunks[args.num_chunks*(c):args.num_chunks*(c + 1)]
+        client_image_files = []
+        for chunk in client_chunks:
+            client_image_files += image_files[num_examples_per_chunk*(chunk):num_examples_per_chunk*(chunk + 1)]
+        np.random.shuffle(client_image_files)
+        for image_file in client_image_files[:int(num_examples_per_chunk*0.9)]:
+            label = int(image_file.split("/")[-2])
+            shutil.copy(image_file, args.data_dir + "clients/c{}/fit/{}/".format(str(c), str(label)))
+        for image_file in client_image_files[int(num_examples_per_chunk*0.9):]:
+            label = int(image_file.split("/")[-2])
+            shutil.copy(image_file, args.data_dir + "clients/c{}/evaluate/{}/".format(str(c), str(label)))
