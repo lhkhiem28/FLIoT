69fff2c207b4f3d745daa9d6798aea0084786fa9
lhkhiem28 <lhkhiem28@gmail.com>
2023-01-25 07:33:36 +0000

    baseline

---
+++ source/runs/run0.sh
@@ -0,0 +1,33 @@
+cd ../main
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=00 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=01 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=08 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=09 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=16 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=17 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=24 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=25 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=32 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=33 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=40 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=41 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=48 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=49 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=56 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=57 &
---
+++ source/runs/run2.sh
@@ -0,0 +1,33 @@
+cd ../main
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=04 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=05 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=12 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=13 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=20 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=21 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=28 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=29 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=36 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=37 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=44 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=45 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=52 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=53 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=60 &
+sleep 3
+nohup python3 client.py --dataset="CIFAR10-64-NonIID" --num_classes=10 --wandb_project="Sim-64-NonIID-CIFAR10" --client_id=61 &
